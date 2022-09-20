11d331b0a25e9bb5d5f909e6e973a97273de7583
GomeXodia <andreas.gomex@gmail.com>
2022-09-20 04:53:36 -0700

    mark-4

--- promise.js
+++ promise.js
@@ -1,22 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = async (sikap) => {
+const promiseOutput = async (emotion) => {
 
   const theaterIXX = await promiseTheaterIXX();
   const theaterVGC = await promiseTheaterVGC();
 
   const allTheater = [...theaterIXX, ...theaterVGC]
 
-  let countSikap = 0;
+  let countE = 0;
 
   allTheater.forEach((film) => {
-    if (film.hasil === sikap) {
-      countSikap++;
+    if (film.hasil === emotion) {
+      countE++;
     }
   })
 
-  return countSikap;
+  return countE;
 
 };
 
